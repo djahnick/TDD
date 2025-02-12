542a0e474fae2c3202943968bb5a1530026345b2
djah97123 <djahnick.gene@efrei.net>
2025-02-12 15:10:39 +0100

    Add .gitignore and remove node_modules from tracking

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+/**
+ * Stub de la fonction solveNQueens.
+ * @param {number} n - Taille de l'échiquier (nombre de dames)
+ * @returns {string[][]} - Tableau de solutions
+ */
+function solveNQueens(n) {
+    return []; // Retourne un tableau vide pour l'instant
+}
+
+// Pour permettre les tests, exportons la fonction
+module.exports = solveNQueens;
---
+++ index.test.js
@@ -0,0 +1,9 @@
+// index.test.js
+
+const solveNQueens = require('./index');
+
+describe('N-Queens Problem', () => {
+    test('La fonction solveNQueens existe', () => {
+        expect(typeof solveNQueens).toBe('function');
+    });
+});
