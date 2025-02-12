aa4d881bcbdc6827739a9dd7780d6fd331dfc5b8
djah97123 <djahnick.gene@efrei.net>
2025-02-12 15:31:35 +0100

    6  isSafe function

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 // index.test.js
 
 const solveNQueens = require('./index');
-const { formatSolution } = require('./index');
+const { formatSolution, isSafe } = require('./index');
 
 describe('N-Queens Problem', () => {
     test('La fonction solveNQueens existe', () => {
@@ -26,5 +26,26 @@ describe('Format Solution', () => {
             "OO#O"
         ];
         expect(formatSolution(queens, 4)).toEqual(expectedBoard);
+    });
+});
+
+describe('isSafe function', () => {
+    test('Retourne true si la position est sûre', () => {
+        // Exemple : Une reine à la ligne 0, colonne 1.
+        const queens = [1];
+        // Pour la ligne 1, colonne 3 ne partage ni la même colonne ni la diagonale.
+        expect(isSafe(queens, 1, 3)).toBe(true);
+    });
+
+    test('Retourne false pour la même colonne', () => {
+        const queens = [1];
+        // Essayer de placer une reine en (1,1) échoue car la colonne 1 est déjà occupée.
+        expect(isSafe(queens, 1, 1)).toBe(false);
+    });
+
+    test('Retourne false pour une position en diagonale', () => {
+        const queens = [1];
+        // Essayer de placer une reine en (1,0) échoue car (0,1) et (1,0) sont en diagonale.
+        expect(isSafe(queens, 1, 0)).toBe(false);
     });
 });
